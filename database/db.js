43d62d142c6f8339bd02c054d56fed3151c57f68
ahmadzakime <146191389+ahmadzakime@users.noreply.github.com>
2023-10-01 15:17:29 +0700

    update database/db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://kuhong-api.herokuapp.com';
+var url = 'https://restapinux-d6d81be12bf3.herokuapp.com';
 try {
-if(url == 'https://kuhong-api.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://restapinux-d6d81be12bf3.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
