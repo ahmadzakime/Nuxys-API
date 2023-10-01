3aa4188a32fbd57ff248db375df5010369b4a12d
ahmadzakime <146191389+ahmadzakime@users.noreply.github.com>
2023-10-01 17:04:10 +0700

    update database/db.js

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nuxysapi:<password>@cluster0.cmvqrmm.mongodb.net/?retryWrites=true&w=majority";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -7,6 +10,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -14,8 +18,10 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
 run().catch(console.dir);
-
 module.exports = db
