e5b84315e9e708909b6f486732f5f76a47c51239
ahmadzakime <146191389+ahmadzakime@users.noreply.github.com>
2023-10-01 17:24:02 +0700

    update database/db.js

--- database/db.js
+++ database/db.js
@@ -1,16 +1,28 @@
-const mongoose = require('mongoose');
 
-MONGO_DB_URI = 'mongodb+srv://nuxysapi:<password>@cluster0.cmvqrmm.mongodb.net/?retryWrites=true&w=majority'
-function connectMongoDb() {
-    mongoose.connect(MONGO_DB_URI, { 
-      useNewUrlParser: true, 
-      useUnifiedTopology: true
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
-      console.log('Succes connect to MONGODB ✅');
-    });
-};
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://nuxysapi:<password>@cluster0.cmvqrmm.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectMongoDb() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+connectMongoDb().catch(console.dir);
 
 module.exports.connectMongoDb = connectMongoDb;
