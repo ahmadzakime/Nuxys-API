c9ab3e2a678b9c4b7103bae92729e27399a058bb
Ahmad Zaki <146191389+ahmadzakime@users.noreply.github.com>
2023-10-04 20:15:49 +0700

    update routes/api.js

--- routes/api.js
+++ routes/api.js
@@ -1019,7 +1019,7 @@ router.get('/download/fb', async (req, res, next) => {
 	if(apikeyInput !== `${key}`) return res.sendFile(invalidKey)
     if (!url) return res.json({ status : false, creator : `${creator}`, message : "Masukan parameter url"})
 
-       bch.snapsave(url).then(async (data) => {
+       bch.instagramdl(url).then(async (data) => {
 	res.json({
 	status: true,
 	creator: `${creator}`,
